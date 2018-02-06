49e4dfd606ab063e6a81c0305639b7039e034c23
Hipocrady <napa.nguyenhoang@gmail.com>
2018-02-06 18:29:55 +0700

    update trang them san pham

--- front-end/admin/js/add-product.js
+++ front-end/admin/js/add-product.js
@@ -1,11 +1,10 @@
 var PRODUCT_API = "http://localhost:3000/_api/v1/products";
-
+var FILE_UPLOAD_URL = "http://localhost:3000/_api/v1/images";
 $(document).ready(function(){
 	var isValidProductName=false;
-	var isValidProductId=false;
+	var isValidProductId=true;
 	var isValidProductPrice=false;
 	var isValidProductDescription=false;
-	var isValidProductImage=false;
 
 	function checkIsValidProductName(){
 		var pName = $('[name="pName"]').val();
@@ -20,31 +19,31 @@ $(document).ready(function(){
 		}
 	}
 
-	function checkIsValidProductId(){
-		var pId = $('[name="pId"]').val();
-		var pCategory = $('[name="pCategory"]:checked').val();
-		var pCategoryId = pId.charAt(0);
-		if(pId != undefined && pId != null && pId.length == 4 && (pCategoryId == "G" || pCategoryId == "R" || pCategoryId == "H")){
-			$("[name='pId']").next().addClass("text-success").removeClass("text-danger");
-			$("[name='pId']").next().text("Mã sản phẩm hợp lệ.");
-			isValidProductId=true;
-			if(pCategoryId == 'H'){
-				$("#pCategory01").prop("checked", true);
-				$('#pTypeBlock').css("display", "none");
-			} else if(pCategoryId == 'G'){
-					$("#pCategory02").prop("checked", true);
-					$('#pTypeBlock').css("display", "block");
-				} else {
-					$("#pCategory03").prop("checked", true);
-					$('#pTypeBlock').css("display", "block");
-				}
-			jQuery("#admin-add-product-form input[id^=pCategory]:radio").attr('disabled',true);
-		}else{
-			isValidProductId=false;
-			$("[name='pId']").next().addClass("text-danger").removeClass("text-success");
-			$("[name='pId']").next().text("Mã sản phẩm phải bằng 4 kí tự và bắt đầu bằng R, G hoặc H.");
-		}
-	}
+	// function checkIsValidProductId(){
+	// 	var pId = $('[name="pId"]').val();
+	// 	var pCategory = $('[name="pCategory"]:checked').val();
+	// 	var pCategoryId = pId.charAt(0);
+	// 	if(pId != undefined && pId != null && pId.length == 4 && (pCategoryId == "G" || pCategoryId == "R" || pCategoryId == "H")){
+	// 		$("[name='pId']").next().addClass("text-success").removeClass("text-danger");
+	// 		$("[name='pId']").next().text("Mã sản phẩm hợp lệ.");
+	// 		isValidProductId=true;
+	// 		if(pCategoryId == 'H'){
+	// 			$("#pCategory01").prop("checked", true);
+	// 			$('#pTypeBlock').css("display", "none");
+	// 		} else if(pCategoryId == 'G'){
+	// 				$("#pCategory02").prop("checked", true);
+	// 				$('#pTypeBlock').css("display", "block");
+	// 			} else {
+	// 				$("#pCategory03").prop("checked", true);
+	// 				$('#pTypeBlock').css("display", "block");
+	// 			}
+	// 		jQuery("#admin-add-product-form input[id^=pCategory]:radio").attr('disabled',true);
+	// 	}else{
+	// 		isValidProductId=false;
+	// 		$("[name='pId']").next().addClass("text-danger").removeClass("text-success");
+	// 		$("[name='pId']").next().text("Mã sản phẩm phải bằng 4 kí tự và bắt đầu bằng R, G hoặc H.");
+	// 	}
+	// }
 
 	function checkIsValidProductPrice(){
 		var pPrice = $('[name="pPrice"]').val();
@@ -71,77 +70,7 @@ $(document).ready(function(){
 			isValidProductDescription=true;
 		}
 	}
-
-	function checkIsValidProductImage(){
-		var pImage = $('#pImage').val();
-		if(!pImage){		
-			isValidProductImage=false;
-			$("#pImage").next().addClass("text-danger").removeClass("text-success");
-			$("#pImage").next().text("Vui lòng tải ảnh sản phẩm.");
-		}else{
-			$("#pImage").next().addClass("text-success").removeClass("text-danger");
-			$("#pImage").next().text("Ảnh sản phẩm hợp lệ.");
-			isValidProductImage=true;
-		}
-	}
-
-	// Lấy file ảnh ra cho vào preview
-	function readURL(input) {
-	  if (input.files && input.files[0]) {
-	    var reader = new FileReader();
-	    reader.onload = function (e) {
-	      $('#preview')
-	        .attr('src', e.target.result)
-	        .width(150);
-	    };
-	    reader.readAsDataURL(input.files[0]);
-	  }
-	}
-
-	var pName = $('[name="pName"]').val();
-	var pId = $('[name="pId"]').val();
-	var pPrice = $('[name="pPrice"]').val();
-	var pDescription = $('[name="pDescription"]').val();
-	var pCategory = $('[name="pCategory"]:checked').val();
-	var pType = $('[name="pType"]:checked').val();
-	var status = $('[name="status"]:checked').val();
-	var pImage = $('#pImage').val();
-
-	// Kiểm tra validate sản phẩm
-	$("[name='pName']").keydown(function(event){
-		var pName = $('[name="pName"]').val();
-		if(event.keyCode==9){
-			checkIsValidProductName();
-		}
-	});
-
-	$("[name='pId']").keydown(function(event){
-		var pId = $('[name="pId"]').val();
-		var pCategory = $('[name="pCategory"]:checked').val();
-		if(event.keyCode==9){
-			checkIsValidProductId();
-		}
-	});
-
-	$("[name='pPrice']").keydown(function(event){
-		var pPrice = $('[name="pPrice"]').val();
-		if(event.keyCode==9){
-			checkIsValidProductPrice();
-		}	
-	});
-
-	$("[name='pDescription']").keydown(function(event){
-		var pDescription = $('[name="pDescription"]').val();
-		if(event.keyCode==9){
-			checkIsValidProductDescription();
-		}
-	});
-
-	$("#pImage").mouseup(function(event){
-		var pImage = $('#pImage').val();
-		checkIsValidProductImage();
-	});
-
+	
 	// Lấy thông tin sản phẩm ra
 	function getInfo(id){
 		$.ajax({
@@ -164,9 +93,21 @@ $(document).ready(function(){
 			}
 		});
 	}
-
 	// Thêm sản phẩm mới
 	function addProduct(){
+
+	var pName = $('[name="pName"]').val();
+	var pId = $('[name="pId"]').val();
+	var pPrice = $('[name="pPrice"]').val();
+	var pDescription = $('[name="pDescription"]').val();
+	var pCategory = $('[name="pCategory"]:checked').val();
+	var pType = $('[name="pType"]:checked').val();
+	var status = $('[name="status"]:checked').val();
+	var pImage = $('#pImage').val();
+	// checkIsValidProductId();
+	checkIsValidProductName();
+	checkIsValidProductDescription();
+	checkIsValidProductPrice();
 		var product = {
 			'pName': pName,
 			'pId': pId,
@@ -180,8 +121,9 @@ $(document).ready(function(){
 
 		var api_url = PRODUCT_API;
 		var method = 'POST';
+		console.log(isValidProductName,isValidProductId,isValidProductPrice,isValidProductDescription)
 		if(isValidProductName && isValidProductId && isValidProductPrice 
-			&& isValidProductDescription && isValidProductImage){
+			&& isValidProductDescription){
 			$.ajax({
 				url: api_url,
 				type: method,
@@ -197,15 +139,28 @@ $(document).ready(function(){
 			});
 		} else {
 			checkIsValidProductName();
-			checkIsValidProductId();
+			// checkIsValidProductId();
 			checkIsValidProductPrice();
 			checkIsValidProductDescription();
-			checkIsValidProductImage();
 		}
 	};
 
 	// Update thông tin sản phẩm có sẵn
 	function updateProduct(id){
+
+		var pName = $('[name="pName"]').val();
+		var pId = $('[name="pId"]').val();
+		var pPrice = $('[name="pPrice"]').val();
+		var pDescription = $('[name="pDescription"]').val();
+		var pCategory = $('[name="pCategory"]:checked').val();
+		var pType = $('[name="pType"]:checked').val();
+		var status = $('[name="status"]:checked').val();
+		var pImage = $('#pImage').val();
+		// checkIsValidProductId();
+		checkIsValidProductName();
+		checkIsValidProductDescription();
+		checkIsValidProductPrice();
+
 		var product = {
 			'pName': pName,
 			'pId': pId,
@@ -220,7 +175,7 @@ $(document).ready(function(){
 		var api_url = PRODUCT_API + "/" + id;
 		var method = "PUT";
 		if(isValidProductName && isValidProductId && isValidProductPrice 
-			&& isValidProductDescription && isValidProductImage){
+			&& isValidProductDescription){
 			$.ajax({
 				url: api_url,
 				type: method,
@@ -236,10 +191,9 @@ $(document).ready(function(){
 			});
 		} else {
 			checkIsValidProductName();
-			checkIsValidProductId();
+			// checkIsValidProductId();
 			checkIsValidProductPrice();
 			checkIsValidProductDescription();
-			checkIsValidProductImage();
 		}
 	};
 
@@ -260,5 +214,37 @@ $(document).ready(function(){
 		if (!results) return null;
 		if (!results[2]) return '';
 		return decodeURIComponent(results[2].replace(/\+/g, " "));
-	}
+	};
+	$("#pImage").change(function (e){						
+		var data = new FormData();
+		data.append('myFile', e.target.files[0]);
+		$.ajax({
+			url: FILE_UPLOAD_URL,
+			type: "POST",
+			data: data,
+			cache: false,
+		    contentType: false,
+		    processData: false,
+			success: function(response){
+				console.log(response)										
+				$('#preview').attr('src', response);
+				$('[name="avatarUrl"]').val(response);
+			},
+			error: function(response, message){
+				alert('Có lỗi xảy ra. ' + message);
+			}
+		});
+	});
 });
+// Lấy file ảnh ra cho vào preview
+// function readURL(input) {
+// 	if (input.files && input.files[0]) {
+// 	  var reader = new FileReader();
+// 	  reader.onload = function (e) {
+// 		$('#preview')
+// 		  .attr('src', e.target.result)
+// 		  .width(150);
+// 	  };
+// 	  reader.readAsDataURL(input.files[0]);
+// 	}
+//   }
